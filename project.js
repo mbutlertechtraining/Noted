5d831a19d4219cf89480101987cb0d02e42b6bce
Mario Butler <mbutlertechtraining@DESKTOP-EHPAM6P>
2022-03-24 23:06:13 -0400

    new branch

---
+++ project.js
@@ -0,0 +1,26 @@
+//$(document).ready(function ()) {
+
+var container = document.querySelector('container')
+var image = document.querySelector('#music-image')
+
+const input = document.querySelector('#container')
+const submitBtn = document.querySelector('#submit')
+const key = 
+const url = https:
+
+// const url = 'https://musixmatch
+
+submitBtn.addEventListener('click', function(event) {
+    event.preventDefault()
+    console.log(input.value)
+    fetch(url + input.value).then(Response => { 
+        return response.json()
+    }).then(function (data) {
+        console.log(data.result[0].image)
+        let music = data.result.image
+
+    })  
+})
+
+
+//}) 
